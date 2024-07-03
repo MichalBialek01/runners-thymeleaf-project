a374c7b72f0f86067ff5dfa6b0083978397c7f10
Michał Białek <kontakt.michalbialek@gmail.com>
2024-07-03 12:53:44 +0200

    Initial commit - Prepared Spring Initializr dependencies, and configuration to connect to database

---
+++ src/main/java/pl/bialek/runnersthymeleafproject/RunnersThymeleafProjectApplication.java
@@ -0,0 +1,13 @@
+package pl.bialek.runnersthymeleafproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunnersThymeleafProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RunnersThymeleafProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/bialek/runnersthymeleafproject/RunnersThymeleafProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.bialek.runnersthymeleafproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RunnersThymeleafProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
